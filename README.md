da02064d77ce3daedeb05b8d5185eeb62378492c
KashifCh_eth <103598065+KashifCh-eth@users.noreply.github.com>
2023-08-10 01:25:15 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+# Vite Starter Kit 
+
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules. just Need
+
+# For Install Node_modules 
+
+Yarn / Npm Install
+
+# For Run 
+
+Yarn Run Dev / npm Run Dev
+
+
+
+ 
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+   <h1>React Vite Starter Kit</h1>
+   <a href="https://github.com/KashifCh">Github: Github.com/KashifCh</a><br></br>
+   <a href="https://www.linkedin.com/in/kashif-choudary/">Linkedin: linkedin.com/in/kashif-choudary/</a>
+  </>
+  )
+}
+
+export default App
